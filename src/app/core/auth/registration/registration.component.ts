ca20633c7768af943703eebdc110fb83db9195f7
piermusa <piermusa@gmail.com>
2023-11-20 18:25:20 +0100

    validetion

--- src/app/core/auth/registration/registration.component.ts
+++ src/app/core/auth/registration/registration.component.ts
@@ -1,48 +1,88 @@
-import { Component } from "@angular/core";
+import { Component, ElementRef, ViewChild } from "@angular/core";
+import { FormBuilder, FormGroup, Validators, AbstractControl, ValidationErrors } from "@angular/forms";
 import { Router } from "@angular/router";
 import { getAuth, createUserWithEmailAndPassword, UserCredential } from "firebase/auth";
 import { collection, addDoc } from "firebase/firestore"; 
 import { db } from "src/app/configurazioneFirebase";
-
 
 @Component({
   selector: 'app-registration',
   templateUrl: './registration.component.html',
   styleUrls: ['./registration.component.scss']
 })
-
 export class RegistrationComponent {
-  constructor(private router: Router) {}
-nome: string = '';
-cognome: string = '';
-  email: string = '';
-  password: string = '';
- id: string = ''
+  reactiveForm: FormGroup;
 
+  @ViewChild('passwordInput') passwordInput: ElementRef | undefined;
+  @ViewChild('confirmPasswordInput') confirmPasswordInput: ElementRef | undefined;
+
+  constructor(private fb: FormBuilder, private router: Router) {
+    this.reactiveForm = this.fb.group({
+      id: [''],
+      nome: ['', Validators.required],
+      cognome: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      confirmPassword: ['', Validators.required],
+    });
+  }
+
+  ngAfterViewInit() {
+    // Aggiungi gli ascoltatori agli eventi degli input
+    if (this.passwordInput && this.confirmPasswordInput) {
+      this.passwordInput.nativeElement.addEventListener('input', () => {
+        this.validatePassword();
+      });
+
+      this.confirmPasswordInput.nativeElement.addEventListener('input', () => {
+        this.validatePassword();
+      });
+    }
+  }
+
+  validatePassword(): void {
+    const passwordControl = this.reactiveForm.get('password');
+    const confirmPasswordControl = this.reactiveForm.get('confirmPassword');
   
- 
- 
- 
- 
- registerUser() {
-    const auth = getAuth();
-    createUserWithEmailAndPassword(auth, this.email, this.password)
-      .then(async (userCredential: UserCredential) => {
-        // Utente registrato con successo
-        const user = userCredential.user;
-        // Puoi accedere a 'this' qui
-      
+    if (passwordControl && confirmPasswordControl) {
+      const password = passwordControl.value;
+      const confirmPassword = confirmPasswordControl.value;
+  
+      if (password === confirmPassword) {
+        confirmPasswordControl.setErrors(null);
+      } else {
+        confirmPasswordControl.setErrors({ passwordsNotMatch: true });
+      }
+    }
+  }
+  
+  async registerUser() {
+    this.validatePassword(); // Chiamare validatePassword prima di procedere
+
+    if (this.reactiveForm.valid) {
+      const auth = getAuth();
+      const email = this.reactiveForm.value.email;
+      const password = this.reactiveForm.value.password;
+
       try {
+        const userCredential: UserCredential = await createUserWithEmailAndPassword(auth, email, password);
+        const user = userCredential.user;
+
         const docRef = await addDoc(collection(db, "users"), {
-          nome: this.nome,
-          cognome:this.cognome,
-          email: this.email,
-          password:this.password,
+          nome: this.reactiveForm.value.nome,
+          cognome: this.reactiveForm.value.cognome,
+          email: email,
+          password: password,
           id: user.uid
         });
+
         console.log("Document written with ID: ", docRef.id);
+        // Puoi aggiungere qui la navigazione alla pagina successiva
+        this.router.navigate(['/success']);
       } catch (e) {
-        console.error("Error adding document: ", e);
+        console.error("Registration error: ", e);
+        // Puoi gestire l'errore e visualizzare un messaggio appropriato
       }
-    })}
+    }
+  }
 }

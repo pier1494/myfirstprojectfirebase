76706932e45e5eb847a7f268d8e2fca9a4c9a11c
Francesco Corvino <francesco.corvino@linksmt.it>
2023-12-04 15:40:13 +0100

    aggiunto corpo componente principale

--- src/app/features/recensioni/recensioni.module.ts
+++ src/app/features/recensioni/recensioni.module.ts
@@ -1,15 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RecensioniComponent } from './recensioni.component';
+import { RouterModule, Routes } from '@angular/router';
 
 
+
+const routes: Routes = [
+  { path: '', component: RecensioniComponent }
+];
 
 @NgModule({
   declarations: [
     RecensioniComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(routes)    
   ]
 })
 export class RecensioniModule { }

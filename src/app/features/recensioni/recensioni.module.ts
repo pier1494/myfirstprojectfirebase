b4d6d47e9d8412812892efed8a832b78b88cdd7c
Francesco Corvino <francesco.corvino@linksmt.it>
2023-12-04 15:29:38 +0100

    creato modulo-componente

---
+++ src/app/features/recensioni/recensioni.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RecensioniComponent } from './recensioni.component';
+
+
+
+@NgModule({
+  declarations: [
+    RecensioniComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class RecensioniModule { }

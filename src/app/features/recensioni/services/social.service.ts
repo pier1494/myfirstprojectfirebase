0015a8f5fb7d43bc668d15d07bba8038763d6877
Francesco Corvino <francesco.corvino@linksmt.it>
2023-12-20 10:40:28 +0100

    Aggiunta cache social.service

--- src/app/features/recensioni/services/social.service.ts
+++ src/app/features/recensioni/services/social.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { collection, doc, getDoc, getDocs, query, where } from "firebase/firestore";
-import { Observable, catchError, combineLatest, forkJoin, map, switchMap } from "rxjs";
+import { Observable, catchError, combineLatest, forkJoin, map, of, switchMap } from "rxjs";
 import { db } from "src/app/configurazioneFirebase";
 import { LoaderService } from 'src/app/loader.service';
 
@@ -9,14 +9,26 @@ import { LoaderService } from 'src/app/loader.service';
 })
 export class SocialService {
 
+  private usersInfo: any = {};
+
   constructor(private loading: LoaderService) { }
+
+  resetUsers() {
+    this.usersInfo = {};
+  }
 
   getUserInfo(userId: string): Observable<any> {
     this.loading.showLoader()    
+    if (this.usersInfo[userId]) {
+      console.log("RECUPERO DALLA CACHE " + userId );
+      return of(this.usersInfo[userId]);
+    }
     return new Observable<any>(observer => {
       getDoc(doc(db, 'users', userId)).then(snapshot => {
+        console.log("RECUPERO DALLA WEB " + userId );
         const userData = snapshot.data();
         console.log("PASSO DALL'OBS: ", userData);
+        this.usersInfo[userId] = userData;
         observer.next(userData);
         observer.complete();
       }).catch(error => {
